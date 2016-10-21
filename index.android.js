c900f1cc77d5c40a128822c41b831830183e4ddf
Benjamin Jack <benjaminjack@gmail.com>
2016-10-21 10:30:44 -0400

    Render basic Card listing

--- index.android.js
+++ index.android.js
@@ -1,53 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component, Image, Navigator } from 'react';
+import { Container, Header, Icon, Title, Content, Card, CardItem, Text, Thumbnail, Button } from 'native-base';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
 export default class TwitterTwitt extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+	render() {
+		return (
+			<Container>
+				<Header>
+					<Button transparent>
+						<Icon name='ios-arrow-back' />
+					</Button>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+					<Title>Header</Title>
+					<Button transparent>
+						<Icon name='ios-menu' />
+					</Button>
+				</Header>
+				<Content>
+					<Card>
+						<CardItem >                       
+							<Text>NativeBase</Text>
+							<Text note>April 15, 2016</Text>
+						</CardItem>
+						<CardItem cardBody>
+							<Text>
+								//Your text here
+							</Text>
+							<Button transparent textStyle={{color: '#87838B'}}>
+								389 Stars
+							</Button>
+						</CardItem>
+					</Card>
+				</Content>
+			</Container>
+		);
+	}
+}
 
 AppRegistry.registerComponent('TwitterTwitt', () => TwitterTwitt);
