1f50dd3a7d0690242e6f3a4731b84f3009a4f067
Benjamin Jack <benjaminjack@gmail.com>
2016-11-10 17:50:46 -0500

    Choose users at random to display

--- twittlist.js
+++ twittlist.js
@@ -1,6 +1,6 @@
 import React, { Component, Navigator } from 'react';
 import { Image } from 'react-native';
-import { Container, Header, Icon, Title, Content, Card, CardItem, Text, Thumbnail, Button } from 'native-base';
+import { Container, Header, Icon, Title, Content, Card, CardItem, Text, Thumbnail, Button, List, ListItem } from 'native-base';
 
 var base64 = require('base-64');
 var settings = require('./settings.json')
@@ -10,12 +10,25 @@ var key = settings.key
 var secret = settings.secret
 var keysecret = base64.encode(key + ":" + secret)
 
+// http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
 class TwittList extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			handle: main_user,
+			friend_ids: "",
 			users: 	[
 						{
 							'name': 'test',
@@ -34,6 +47,7 @@ class TwittList extends Component {
 			consumer_key: key.toString('base64'),
 		}
 
+		// Get Access Token
 		fetch('https://api.twitter.com/oauth2/token', {
 					method: 'POST',
 					headers: {
@@ -53,7 +67,8 @@ class TwittList extends Component {
 					console.log("TOKEN")
 					console.log(this.state.token)
 
-					fetch(`https://api.twitter.com/1.1/friends/list.json?screen_name=${this.state.handle}`, {
+					// Fetch friends of primary user
+					fetch(`https://api.twitter.com/1.1/friends/ids.json?screen_name=${this.state.handle}`, {
 								method: 'GET',
 								headers: {
 									'Authorization': 'Bearer ' + this.state.token,
@@ -63,9 +78,34 @@ class TwittList extends Component {
 							.then((response) => response.json())
 							.then((responseJson) => {
 								console.log(responseJson)
+
 								this.setState({
-									users: responseJson.users,
+									// Choose N random IDs from IDs returned
+									friend_ids: getRandomSubarray(responseJson.ids, 50),
 								})
+
+										// Fetch user data for those random IDs
+										fetch(`https://api.twitter.com/1.1/users/lookup.json?user_id=${this.state.friend_ids.toString()}`, {
+										method: 'GET',
+										headers: {
+											'Authorization': 'Bearer ' + this.state.token,
+											'Accept': 'application/json',
+										},
+									})
+									.then((response) => response.json())
+									.then((responseJson) => {
+										console.log(responseJson)
+										
+										// Save resulting user data for those random IDs
+										this.setState({
+											users: responseJson,
+										})
+										
+									})
+									.catch((error) => {
+										console.error(error);
+									});	
+
 							})
 							.catch((error) => {
 								console.error(error);
@@ -80,14 +120,30 @@ class TwittList extends Component {
 		
 		var cards = this.state.users.map(function(user, i){
 			
+			if (user.description) {
+				description = <CardItem key={i+2}>
+								<Text style={{fontStyle: 'italic'}}>{user.description}</Text>
+							</CardItem>
+			} else {
+				description = null;
+			}
+
+			if (user.profile_banner_url) {
+				banner = <CardItem key={i+3}>
+							<Image style={{ resizeMode: 'cover' }} source={{uri: user.profile_banner_url + "/mobile_retina" }} />
+						</CardItem>
+			} else {
+				banner = null;
+			}
+
 			if (user.status) {
-				status = <CardItem cardBody key={i+4}>
+				status = <CardItem key={i+4}>
 							<Text>{user.status.text}{"\n"}
-							{user.status.created_at}
+								{user.status.created_at} / RT {user.status.retweet_count} / FV {user.status.favorite_count}
 							</Text>
 						</CardItem>
 			} else {
-				status = <CardItem cardBody><Text>{"\n"}{"\n"}{"\n"}</Text></CardItem>;
+				status = null;
 			}
 
 			return  (
@@ -97,13 +153,9 @@ class TwittList extends Component {
 						<Text style={{fontWeight: 'bold'}}>{user.name}</Text>
 						<Text note>@{user.screen_name}</Text>
 					</CardItem>
-					<CardItem key={i+2}>
-						<Text>{user.description}</Text>
-					</CardItem>
-					<CardItem key={i+3}>
-						<Image style={{ resizeMode: 'cover' }} source={{uri: user.profile_banner_url + "/mobile_retina" }} /> 
-					</CardItem>
+					{description}
 					{status}
+					{banner}
 				</Content>
 
 			);
